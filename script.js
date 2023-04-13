723a47a845761211c348bf83903ca87a83209a62
Musangamfura Emmanuel <musangamfure@gmail.com>
2023-04-13 16:46:43 +0100

    Add HTML, CSS and JS file

--- script.js
+++ script.js
@@ -1,18 +1,48 @@
-const humbargerMenu = document.querySelector('.toggle-btn');
+// ================Mobile Menu===============
+const humbargerMenu = document.querySelector(".toggle-btn");
 
-const navLinks = document.querySelector('.nav-links');
+const navLinks = document.querySelector(".nav-links");
 
-const closeIcon = document.querySelector('.close-icon');
+const closeIcon = document.querySelector(".close-icon");
 
-const navLink = document.querySelectorAll('.nav-link');
+const navLink = document.querySelectorAll(".nav-link");
 
-humbargerMenu.addEventListener('click', () => {
-  navLinks.classList.add('show-links');
+humbargerMenu.addEventListener("click", () => {
+  navLinks.classList.add("show-links");
 });
-closeIcon.addEventListener('click', () => {
-  navLinks.classList.remove('show-links');
+closeIcon.addEventListener("click", () => {
+  navLinks.classList.remove("show-links");
 });
 
-navLink.forEach((n) => n.addEventListener('click', () => {
-  navLinks.classList.remove('show-links');
-}));
+navLink.forEach((n) =>
+  n.addEventListener("click", () => {
+    navLinks.classList.remove("show-links");
+  })
+);
+
+// ================ End of Mobile Menu===============
+
+// ========Client side validation============
+
+const email = document.querySelector(".email");
+const form = document.querySelector("#form");
+const error = document.querySelector(".error");
+const sent = document.querySelector(".sent");
+
+function isItUppercase(value) {
+  if (value.match(/^[a-z@.0-9-_]*$/)) {
+    return true;
+  }
+  return false;
+}
+
+form.addEventListener("submit", (e) => {
+  if (isItUppercase(email.value)) {
+    sent.textContent = " Message Sent!";
+  } else {
+    e.preventDefault();
+    error.textContent = "Email should be in lowerCase";
+  }
+});
+
+// =============End of form validation ===========
