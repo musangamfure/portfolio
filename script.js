b879b29ff7aec6d3ab34dd6ebfe34ae49878c06f
Musangamfura Emmanuel <musangamfure@gmail.com>
2023-04-14 22:07:25 +0100

    Update CSS and JS file

--- script.js
+++ script.js
@@ -1,51 +1,47 @@
-const humbargerMenu = document.querySelector(".toggle-btn");
+const humbargerMenu = document.querySelector('.toggle-btn');
 
-const navLinks = document.querySelector(".nav-links");
+const navLinks = document.querySelector('.nav-links');
 
-const closeIcon = document.querySelector(".close-icon");
+const closeIcon = document.querySelector('.close-icon');
 
-const navLink = document.querySelectorAll(".nav-link");
+const navLink = document.querySelectorAll('.nav-link');
 
-humbargerMenu.addEventListener("click", () => {
-  navLinks.classList.add("show-links");
+humbargerMenu.addEventListener('click', () => {
+  navLinks.classList.add('show-links');
 });
-closeIcon.addEventListener("click", () => {
-  navLinks.classList.remove("show-links");
+closeIcon.addEventListener('click', () => {
+  navLinks.classList.remove('show-links');
 });
 
-navLink.forEach((n) =>
-  n.addEventListener("click", () => {
-    navLinks.classList.remove("show-links");
-  })
-);
+navLink.forEach((n) => n.addEventListener('click', () => {
+  navLinks.classList.remove('show-links');
+}));
 
 // =============End of Mobile Menu=========
 
 // =========Local Storage ============
 
-const form = document.querySelector("#form");
-const formName = document.querySelector(".name");
-const formEmail = document.querySelector(".email");
-const formMessage = document.querySelector(".message");
+const form = document.querySelector('#form');
+const formName = document.querySelector('.name');
+const formEmail = document.querySelector('.email');
+const formMessage = document.querySelector('.message');
 
-const formData = JSON.parse(localStorage.getItem("formData")) || {};
+const formData = JSON.parse(localStorage.getItem('formData')) || {};
 
-formName.value = formData.name || "";
-formEmail.value = formData.email || "";
-formMessage.value = formData.message || "";
+formName.value = formData.name || '';
+formEmail.value = formData.email || '';
+formMessage.value = formData.message || '';
 
-form.addEventListener("input", () => {
+form.addEventListener('input', () => {
   const newFormData = {
     name: formName.value,
     email: formEmail.value,
     message: formMessage.value,
   };
 
-  localStorage.setItem("formData", JSON.stringify(newFormData));
+  localStorage.setItem('formData', JSON.stringify(newFormData));
 });
 
-navLink.forEach((n) =>
-  n.addEventListener("click", () => {
-    navLinks.classList.remove("show-links");
-  })
-);
+navLink.forEach((n) => n.addEventListener('click', () => {
+  navLinks.classList.remove('show-links');
+}));
